69519ba8d42c3af89682014563709462b09cbfed
Stuart Auld <sauld@cozero.com.au>
2016-01-21 21:14:53 +1100

    last update was bugfix only

--- lib/lita-coffee/version.rb
+++ lib/lita-coffee/version.rb
@@ -1,6 +1,6 @@
 module LitaCoffee
   # lita-coffee version
-  VERSION = '0.2.0'
+  VERSION = '0.1.3'
   # lita-github version split amongst different revisions
   MAJOR_VERSION, MINOR_VERSION, REVISION = VERSION.split('.').map(&:to_i)
 end
